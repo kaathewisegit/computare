e220199ef71b0759043dc96c280b75a480ab67e3
Andrej Kolčin <self@kaathewise.net>
2026-06-12 14:13:14 +0300

    [special/tests] gamma: tweak `ln_gamma` tests

--- crates/special/tests/it/gamma.rs
+++ crates/special/tests/it/gamma.rs
@@ -36,9 +36,9 @@ fn gamma_10_33() {
     arbtest(|u| compare_gamma(f64_range(u, 5.0, 33.0)?, 1e-14));
 }
 
-fn compare_ln_gamma(f: f64, relative: f64) -> Result<()> {
-    let rug_gamma = Float::with_val(PREC, f).ln_gamma().az::<f64>();
-    let my_gamma = ln_gamma(f);
+fn compare_ln_gamma(x: f64, relative: f64) -> Result<()> {
+    let rug_gamma = Float::with_val(PREC, x).ln_gamma().az::<f64>();
+    let my_gamma = ln_gamma(x);
 
     assert_almost_eq!(my_gamma, rug_gamma, relative = relative);
     Ok(())
@@ -46,12 +46,13 @@ fn compare_ln_gamma(f: f64, relative: f64) -> Result<()> {
 
 #[test]
 fn ln_gamma_unit() {
-    arbtest(|u| compare_ln_gamma(f64_unit(u)?, 2e-10));
+    arbtest(|u| compare_ln_gamma(f64_unit(u)?, 1e-10));
 }
 
 #[test]
 fn ln_gamma_1_2() {
-    arbtest(|u| compare_ln_gamma(f64_range(u, 1.0, 5.0)?, 1e-11));
+    // 0x8129fc7700010000
+    arbtest(|u| compare_ln_gamma(f64_range(u, 1.0, 5.0)?, 1e-10));
 }
 
 #[test]
